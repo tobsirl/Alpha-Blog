1ebe8b6a4c22bfc45f9c9ef51969c77fd3df5434
Paul Tobin <tobster111@gmail.com>
2021-02-02 17:36:37 +0000

    Save the article in the database and show to the user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,5 +14,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
   end
 end
