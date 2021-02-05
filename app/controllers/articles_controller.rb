8a4a9f91604c9867bb6ba69809618804a71758ae
Paul Tobin <tobster111@gmail.com>
2021-02-05 12:03:19 +0000

    Updated the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,5 +27,12 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 end
